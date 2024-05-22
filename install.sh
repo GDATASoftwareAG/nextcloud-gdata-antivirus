027a90562959dded86c673e7ac18b8ed209cbab9
PT-ATA No One <ata-no-one@gdata.de>
2024-05-22 11:48:30 +0200

    Install script for easy dev testing of release artifact

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+docker stop nextcloud-container
 docker run -d --name nextcloud-container --rm --publish 80:80 --publish 8080:8080 --publish 8443:8443 nextcloud:stable
-wait 6
-docker exec --user www-data -it nextcloud-container php occ maintenance:install --admin-user=admin --admin-pass=
-make build
+echo "Waiting for sunrise..."
+sleep 15
+docker exec --user www-data -it nextcloud-container php occ maintenance:install --admin-user=admin --admin-pass=admin
+make appstore
+tar -xf ./build/artifacts/gdatavaas.tar.gz -C ./build/artifacts
 docker exec --user www-data -it nextcloud-container mkdir apps/gdatavaas
-docker cp ./ nextcloud-container:/var/www/html/apps/gdatavaas
+docker cp ./build/artifacts/gdatavaas nextcloud-container:/var/www/html/apps/gdatavaas
 docker exec --user www-data -it nextcloud-container php occ app:update --all
 docker exec --user www-data -it nextcloud-container php occ app:enable gdatavaas
 
