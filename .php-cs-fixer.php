e74cda6b8a754d029519477f52e6e70560817b1a
PT-ATA No One <ata-no-one@gdata.de>
2024-06-05 09:25:34 +0000

    makes php-cs-fixer work

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,19 +1,20 @@
 <?php
 
-require_once './vendor/autoload.php';
+declare(strict_types=1);
+
+require_once __DIR__.'/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
 
-
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude([
-        'vendor',
-        'node_modules',
-        'build'
-    ])
+	->in(__DIR__)
+	->exclude([
+		'vendor',
+		'node_modules',
+		'build'
+	])
 ;
 
 return (new Config())
-    ->setFinder($finder)
+	->setFinder($finder)
 ;
