b2a037a589a146d74062233532477c840c397571
PT-ATA No One <ata-no-one@gdata.de>
2024-07-03 09:51:58 +0000

    add tag handling commands

---
+++ lib/Command/GetTagIdCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OCA\GDataVaas\Command;
+
+use OCA\GDataVaas\Logging\ConsoleCommandLogger;
+use OCA\GDataVaas\Service\TagService;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetTagIdCommand extends Command {
+	public const TAG_NAME = 'tag-name';
+
+	private LoggerInterface $logger;
+	private TagService $tagService;
+
+	public function __construct(TagService $tagService, LoggerInterface $logger) {
+		parent::__construct();
+		$this->logger = $logger;
+		$this->tagService = $tagService;
+	}
+
+	/**
+	 * @return void
+	 */
+	protected function configure() {
+		$this->setName('gdatavaas:get-tag-id');
+		$this->setDescription('tags all files without tag from this app as unscanned');
+		$this->addArgument(self::TAG_NAME, InputArgument::REQUIRED, "Tag name you want to get the id for.");
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$logger = new ConsoleCommandLogger($this->logger, $output);
+		$tagName = $input->getArgument('tag-name');
+		try {
+			$tag = $this->tagService->getTag($tagName);
+		} catch (TagNotFoundException $e) {
+			$logger->error("Tag not found: ".$tagName." ".$e->getMessage());
+		}
+		$logger->info("tag: ".$tag->getId());
+		return 0;
+	}
+}
