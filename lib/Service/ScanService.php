2bf13efc61d1d5f85dc66e0508d53434e8e5371b
Lennart Dohmann <lennart.dohmann@gdata.de>
2024-07-16 15:31:48 +0200

    File selection for scan queue considers 'Do not scan' and 'Only scan this' settings by checking for scanning permissions before adding a file to the scan queue

--- lib/Service/ScanService.php
+++ lib/Service/ScanService.php
@@ -5,7 +5,6 @@ namespace OCA\GDataVaas\Service;
 use OCA\GDataVaas\AppInfo\Application;
 use OCP\DB\Exception;
 use OCP\IAppConfig;
-use OCP\SystemTag\TagNotFoundException;
 use Psr\Log\LoggerInterface;
 
 class ScanService {
@@ -35,29 +34,11 @@ class ScanService {
 	 * @throws Exception if the database platform is not supported
 	 */
 	public function run(): int {
-		$unscannedTagIsDisabled = $this->appConfig->getValueBool(Application::APP_ID, 'disableUnscannedTag');
-		$quantity = $this->appConfig->getValueInt(Application::APP_ID, 'scanQueueLength', 50);
-
-		$maliciousTag = $this->tagService->getTag(TagService::MALICIOUS);
-		$pupTag = $this->tagService->getTag(TagService::PUP);
-		$cleanTag = $this->tagService->getTag(TagService::CLEAN);
-		$wontScanTag = $this->tagService->getTag(TagService::WONT_SCAN);
-
-		$tagIds = [$maliciousTag->getId(), $cleanTag->getId(), $pupTag->getId(), $wontScanTag->getId()];
-		if ($unscannedTagIsDisabled) {
-			try {
-				array_push($tagIds, ($this->tagService->getTag(TagService::UNSCANNED, false))->getId());
-			} catch (TagNotFoundException) {
-			}
-			$excludedTagIds = [$maliciousTag->getId(), $cleanTag->getId(), $pupTag->getId(), $wontScanTag->getId()];
-			$fileIds = $this->tagService->getFileIdsWithoutTags($excludedTagIds, $quantity);
-		} else {
-			$this->tagService->getTag(TagService::UNSCANNED);
-			$fileIds = $this->tagService->getFileIdsWithTag(TagService::UNSCANNED, $quantity);
-		}
-
-		$this->logger->debug("Scanning files");
-
+        $quantity = $this->appConfig->getValueInt(Application::APP_ID, 'scanQueueLength');
+        
+        $fileIds = $this->getFileIdsToScan($quantity);
+		$this->logger->debug("Scanning " . count($fileIds) . " files");
+        
 		foreach ($fileIds as $fileId) {
 			try {
 				$this->verdictService->scanFileById($fileId);
@@ -65,8 +46,54 @@ class ScanService {
 				$this->logger->error("Failed to scan file with id " . $fileId . ": " . $e->getMessage());
 			}
 		}
-
 		$this->logger->debug("Scanned " . count($fileIds) . " files");
+        
 		return count($fileIds);
 	}
+
+    /**
+     * @param int $quantity
+     * @return array
+     * @throws Exception
+     */
+    private function getFileIdsToScan(int $quantity): array {
+        $unscannedTagIsDisabled = $this->appConfig->getValueBool(Application::APP_ID, 'disableUnscannedTag');
+        
+        $maliciousTag = $this->tagService->getTag(TagService::MALICIOUS);
+        $pupTag = $this->tagService->getTag(TagService::PUP);
+        $cleanTag = $this->tagService->getTag(TagService::CLEAN);
+        $wontScanTag = $this->tagService->getTag(TagService::WONT_SCAN);
+        
+        // TODO: If quantity is higher than the number of files available, this could loop forever
+        $fileIdsAllowedToScan = [];
+        if ($unscannedTagIsDisabled) {
+            $excludedTagIds = [$maliciousTag->getId(), $cleanTag->getId(), $pupTag->getId(), $wontScanTag->getId()];
+            $limit = 50;
+            $offset = 0;
+            while (count($fileIdsAllowedToScan) < $quantity) {
+                $fileIds = $this->tagService->getFileIdsWithoutTags($excludedTagIds, $limit, $offset);
+                foreach ($fileIds as $fileId) {
+                    if ($this->verdictService->isAllowedByAllowAndBlocklist($fileId)) {
+                        $fileIdsAllowedToScan[] = $fileId;
+                    }
+                }
+                $offset += $limit;
+            }
+        } else {
+            $this->tagService->getTag(TagService::UNSCANNED);
+            $limit = 50;
+            $offset = 0;
+            while (count($fileIdsAllowedToScan) < $quantity) {
+                $fileIds = $this->tagService->getFileIdsWithTag(TagService::UNSCANNED, $limit, $offset);
+                foreach ($fileIds as $fileId) {
+                    if ($this->verdictService->isAllowedByAllowAndBlocklist($fileId)) {
+                        $fileIdsAllowedToScan[] = $fileId;
+                    }
+                }
+                $offset += $limit;
+            }
+        }
+        
+        return $fileIdsAllowedToScan;
+    }
 }
