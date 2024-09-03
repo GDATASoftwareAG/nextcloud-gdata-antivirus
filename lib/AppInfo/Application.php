55abd14087b4931e7a20676d503e5b9f5503ef06
Max <Maximilian.Froehling@gdata.de>
2024-09-03 10:43:22 +0200

    Reduce number of activity events generated by tags (#111)
    This introduces a new SystemTagObjectMapperWithoutActivity that uses a
    dummy event system to avoid triggering activities.
    
    Change TagService so that callers can decide if they want activities
    generated for the tags or not. Change defaults to only trigger
    activities for Malicious + Pup tags.
    
    This still generates some events for the initial creation of the system
    tags (done on first use), which is acceptable I think (it's only like 5
    events).
    
    Add a new test for the new behaviour.

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -7,8 +7,11 @@ namespace OCA\GDataVaas\AppInfo;
 use OC\Files\Filesystem;
 use OCA\GDataVaas\AvirWrapper;
 use OCA\GDataVaas\CacheEntryListener;
+use OCA\GDataVaas\Db\DbFileMapper;
 use OCA\GDataVaas\Service\MailService;
+use OCA\GDataVaas\Service\TagService;
 use OCA\GDataVaas\Service\VerdictService;
+use OCA\GDataVaas\SystemTag\SystemTagObjectMapperWithoutActivityFactory;
 use OCP\Activity\IManager;
 use OCP\App\IAppManager;
 use OCP\AppFramework\App;
@@ -20,6 +23,9 @@ use OCP\EventDispatcher\IEventDispatcher;
 use OCP\Files\IHomeStorage;
 use OCP\Files\Storage\IStorage;
 use OCP\IAppConfig;
+use OCP\IDBConnection;
+use OCP\SystemTag\ISystemTagManager;
+use OCP\SystemTag\ISystemTagObjectMapper;
 use OCP\Util;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
@@ -51,6 +57,18 @@ class Application extends App implements IBootstrap {
 		if (file_exists($composerAutoloadFile)) {
 			require_once $composerAutoloadFile;
 		}
+		
+		// Manually register TagService so that we can customize the DI used for $silentTagMapper
+		$context->registerService(TagService::class, function ($c) {
+			$logger = $c->get(LoggerInterface::class);
+			$systemTagManager = $c->get(ISystemTagManager::class);
+			$standardTagMapper = $c->get(ISystemTagObjectMapper::class);
+			$dbConnection = $c->get(IDBConnection::class);
+			$silentTagMapper = SystemTagObjectMapperWithoutActivityFactory::createSilentSystemTagObjectMapper($dbConnection, $systemTagManager);
+			$dbFileMapper = $c->get(DbFileMapper::class);
+			
+			return new TagService($logger, $systemTagManager, $standardTagMapper, $silentTagMapper, $dbFileMapper);
+		}, true);
 
 		CacheEntryListener::register($context);
 
