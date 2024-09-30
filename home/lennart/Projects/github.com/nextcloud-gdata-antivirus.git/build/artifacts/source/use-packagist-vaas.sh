472df009e0bc6b4962618c2b435e6bf97609fdc8
PT-ATA No One <ata-no-one@gdata.de>
2024-09-30 19:30:26 +0000

    adjust scoper config

--- home/lennart/Projects/github.com/nextcloud-gdata-antivirus.git/build/artifacts/source/use-packagist-vaas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf composer.lock
-rm -rf vendor/
-
-if [ -f composer.packagist.vaas.json ]; then
-    mv composer.packagist.vaas.json composer.json
-fi
--- scoper.inc.php
+++ scoper.inc.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 use Isolated\Symfony\Component\Finder\Finder;
 
 // You can do your own things here, e.g. collecting symbols to expose dynamically
@@ -23,119 +22,113 @@ use Isolated\Symfony\Component\Finder\Finder;
 $excludedFiles = [];
 
 return [
-    // The prefix configuration. If a non-null value is used, a random prefix
-    // will be generated instead.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#prefix
-    'prefix' => "OCA\\GDataVaas\\Vendor",
+	// The prefix configuration. If a non-null value is used, a random prefix
+	// will be generated instead.
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#prefix
+	'prefix' => 'OCA\\GDataVaas\\Vendor',
 
-    // The base output directory for the prefixed files.
-    // This will be overridden by the 'output-dir' command line option if present.
-    'output-dir' => null,
+	// The base output directory for the prefixed files.
+	// This will be overridden by the 'output-dir' command line option if present.
+	'output-dir' => null,
 
-    // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
-    // directory. You can however define which files should be scoped by defining a collection of Finders in the
-    // following configuration key.
-    //
-    // This configuration entry is completely ignored when using Box.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#finders-and-paths
-    'finders' => [
-        /*
-        Finder::create()->files()->in('src'),
-        Finder::create()
-            ->files()
-            ->ignoreVCS(true)
-            ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
-            ->exclude([
-                'doc',
-                'test',
-                'test_old',
-                'tests',
-                'Tests',
-                'vendor-bin',
-            ])
-            ->in('vendor'),
-        Finder::create()->append([
-            'composer.json',
-        ]),
-        */
-    ],
+	// By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
+	// directory. You can however define which files should be scoped by defining a collection of Finders in the
+	// following configuration key.
+	//
+	// This configuration entry is completely ignored when using Box.
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#finders-and-paths
+	'finders' => [
+		Finder::create()
+			->files()
+			->ignoreVCS(true)
+			->ignoreDotFiles(true)
+			->exclude([
+				'.devcontainer',
+				'nextcloud-server',
+				'tests',
+				'tmp',
+				'test',
+				'examples'
+			])
+			->in(__DIR__),
+	],
 
-    // List of excluded files, i.e. files for which the content will be left untouched.
-    // Paths are relative to the configuration file unless if they are already absolute
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
-    'exclude-files' => [
-        // 'src/an-excluded-file.php',
-        ...$excludedFiles,
-    ],
+	// List of excluded files, i.e. files for which the content will be left untouched.
+	// Paths are relative to the configuration file unless if they are already absolute
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
+	'exclude-files' => [
+		// 'src/an-excluded-file.php',
+		...$excludedFiles,
+	],
 
-    // PHP version (e.g. `'7.2'`) in which the PHP parser and printer will be configured into. This will affect what
-    // level of code it will understand and how the code will be printed.
-    // If none (or `null`) is configured, then the host version will be used.
-    'php-version' => null,
+	// PHP version (e.g. `'7.2'`) in which the PHP parser and printer will be configured into. This will affect what
+	// level of code it will understand and how the code will be printed.
+	// If none (or `null`) is configured, then the host version will be used.
+	'php-version' => null,
 
-    // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
-    // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
-    // support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
-    // heart contents.
-    //
-    // For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
-    'patchers' => [
-        static function (string $filePath, string $prefix, string $contents): string {
-            // Change the contents here.
-            
-            if (str_ends_with($filePath, 'vendor/netresearch/jsonmapper/src/JsonMapper.php') === true) {
-                $contents = str_replace("namespace $prefix;", "namespace $prefix\\JsonMapper;", $contents);
-            }
+	// When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
+	// original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
+	// support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
+	// heart contents.
+	//
+	// For more see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#patchers
+	'patchers' => [
+		static function (string $filePath, string $prefix, string $contents): string {
+			// Change the contents here.
+			
+			if (str_ends_with($filePath, 'vendor/netresearch/jsonmapper/src/JsonMapper.php') === true) {
+				$contents = str_replace("namespace $prefix;", "namespace $prefix\\JsonMapper;", $contents);
+			}
 
-            if (str_ends_with($filePath, 'vendor/netresearch/jsonmapper/src/JsonMapper/Exception.php') === true) {
-                $contents = str_replace("namespace $prefix;", "namespace $prefix\\JsonMapper;", $contents);
-            }
+			if (str_ends_with($filePath, 'vendor/netresearch/jsonmapper/src/JsonMapper/Exception.php') === true) {
+				$contents = str_replace("namespace $prefix;", "namespace $prefix\\JsonMapper;", $contents);
+			}
 
-            if (str_ends_with($filePath, 'vendor/gdata/vaas/Vaas.php') === true) {
-                $contents = str_replace('use OCA\GDataVaas\Vendor\JsonMapper', 'use OCA\GDataVaas\Vendor\JsonMapper\JsonMapper', $contents);
-            }
+			if (str_ends_with($filePath, 'vendor/gdata/vaas/Vaas.php') === true) {
+				$contents = str_replace('use OCA\GDataVaas\Vendor\JsonMapper', 'use OCA\GDataVaas\Vendor\JsonMapper\JsonMapper', $contents);
+			}
 
-            return $contents;
-        },
-    ],
+			return $contents;
+		},
+	],
 
-    // List of symbols to consider internal i.e. to leave untouched.
-    //
-    // For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#excluded-symbols
-    'exclude-namespaces' => [
-        'Psr',
-        'OCA',
-        'OCP',
-        'OC\Files',
-        'OC\SystemTag',
-        'Symfony',
-        'Icewind'
-    ],
-    'exclude-classes' => [
-        // 'ReflectionClassConstant',
-    ],
-    'exclude-functions' => [
-    ],
-    'exclude-constants' => [
-        // 'STDIN',
-    ],
+	// List of symbols to consider internal i.e. to leave untouched.
+	//
+	// For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#excluded-symbols
+	'exclude-namespaces' => [
+		'Psr',
+		'OCA',
+		'OCP',
+		'OC\Files',
+		'OC\SystemTag',
+		'Symfony',
+		'Icewind'
+	],
+	'exclude-classes' => [
+		'OC',
+	],
+	'exclude-functions' => [
+	],
+	'exclude-constants' => [
+		// 'STDIN',
+	],
 
-    // List of symbols to expose.
-    //
-    // For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#exposed-symbols
-    'expose-global-constants' => false,
-    'expose-global-classes' => false,
-    'expose-global-functions' => false,
-    'expose-namespaces' => [
-        // 'Acme\Foo'                     // The Acme\Foo namespace (and sub-namespaces)
-        // '~^PHPUnit\\\\Framework$~',    // The whole namespace PHPUnit\Framework (but not sub-namespaces)
-        // '~^$~',                        // The root namespace only
-        // '',                            // Any namespace
-    ],
-    'expose-classes' => [],
-    'expose-functions' => [],
-    'expose-constants' => [],
+	// List of symbols to expose.
+	//
+	// For more information see: https://github.com/humbug/php-scoper/blob/master/docs/configuration.md#exposed-symbols
+	'expose-global-constants' => false,
+	'expose-global-classes' => false,
+	'expose-global-functions' => false,
+	'expose-namespaces' => [
+		// 'Acme\Foo'                     // The Acme\Foo namespace (and sub-namespaces)
+		// '~^PHPUnit\\\\Framework$~',    // The whole namespace PHPUnit\Framework (but not sub-namespaces)
+		// '~^$~',                        // The root namespace only
+		// '',                            // Any namespace
+	],
+	'expose-classes' => [],
+	'expose-functions' => [],
+	'expose-constants' => [],
 ];
