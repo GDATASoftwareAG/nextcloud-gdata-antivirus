e746c476363c2169f4d9c57248977ebc05f38aee
PT-ATA No One <ata-no-one@gdata.de>
2024-10-01 19:39:22 +0000

    add memory.ini to top

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -6,6 +6,8 @@ $(composer config home)/vendor/bin/php-scoper completion bash >> $HOME.bash_comp
 echo "export PATH=$(composer config home)/vendor/bin/:\$PATH" >> $HOME/.bashrc
 export PATH=$(composer config home)/vendor/bin/:$PATH
 
+cp memory.ini /usr/local/etc/php/conf.d/memory.ini
+
 if [[ "$IS_CI" == "true" ]]; then
     exit 0
 fi
@@ -14,7 +16,6 @@ sudo bash -c "docker completion bash > /usr/share/bash-completion/completions/do
 sudo bash -c "composer completion bash > /usr/share/bash-completion/completions/composer"
 sudo bash -c "npm completion > /usr/share/bash-completion/completions/npm"
 sudo cp xdebug.local.ini /usr/local/etc/php/conf.d/xdebug.ini
-sudo cp memory.ini /usr/local/etc/php/conf.d/memory.ini
 
 echo ". /usr/share/bash-completion/bash_completion" >> /home/vscode/.bashrc
 
