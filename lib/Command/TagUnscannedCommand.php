322c5f8ccc7e3c56b2eb25bc7bfd1d69871e3340
Kevin Heise <kevin.heise@gdata.de>
2026-06-19 12:47:28 +0200

    inject TagUnscannedService directly  instead other deps

--- lib/Command/TagUnscannedCommand.php
+++ lib/Command/TagUnscannedCommand.php
@@ -7,30 +7,19 @@
 namespace OCA\GDataVaas\Command;
 
 use OCA\GDataVaas\Logging\ConsoleCommandLogger;
-use OCA\GDataVaas\Service\FileService;
-use OCA\GDataVaas\Service\TagService;
 use OCA\GDataVaas\Service\TagUnscannedService;
 use OCP\DB\Exception;
-use OCP\IAppConfig;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class TagUnscannedCommand extends Command {
-	private TagUnscannedService $tagUnscannedService;
-	private LoggerInterface $logger;
-
 	public function __construct(
-		IAppConfig $appConfig,
-		TagService $tagService,
-		FileService $fileService,
-		LoggerInterface $logger,
+		private TagUnscannedService $tagUnscannedService,
+		private LoggerInterface $logger,
 	) {
 		parent::__construct();
-		$this->logger = $logger;
-
-		$this->tagUnscannedService = new TagUnscannedService($logger, $tagService, $fileService, $appConfig);
 	}
 
 	/**
