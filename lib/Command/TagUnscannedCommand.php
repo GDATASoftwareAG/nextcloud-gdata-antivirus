043f73cca4958eb8845ec7681836588207680ef0
Kevin Heise <kevin.heise@gdata.de>
2026-06-19 09:16:48 +0200

    enhance file handling in TagUnscannedService and ScanService
    - Added FileService dependency to TagUnscannedService for improved file ID validation.
    - Implemented removal of stale VaaS tags in ScanService when files are not found.
    - Updated DbFileMapper to use distinct file IDs for better query accuracy.

--- lib/Command/TagUnscannedCommand.php
+++ lib/Command/TagUnscannedCommand.php
@@ -7,6 +7,7 @@
 namespace OCA\GDataVaas\Command;
 
 use OCA\GDataVaas\Logging\ConsoleCommandLogger;
+use OCA\GDataVaas\Service\FileService;
 use OCA\GDataVaas\Service\TagService;
 use OCA\GDataVaas\Service\TagUnscannedService;
 use OCP\DB\Exception;
@@ -20,11 +21,16 @@ class TagUnscannedCommand extends Command {
 	private TagUnscannedService $tagUnscannedService;
 	private LoggerInterface $logger;
 
-	public function __construct(IAppConfig $appConfig, TagService $tagService, LoggerInterface $logger) {
+	public function __construct(
+		IAppConfig $appConfig,
+		TagService $tagService,
+		FileService $fileService,
+		LoggerInterface $logger,
+	) {
 		parent::__construct();
 		$this->logger = $logger;
 
-		$this->tagUnscannedService = new TagUnscannedService($logger, $tagService, $appConfig);
+		$this->tagUnscannedService = new TagUnscannedService($logger, $tagService, $fileService, $appConfig);
 	}
 
 	/**
--- lib/Db/DbFileMapper.php
+++ lib/Db/DbFileMapper.php
@@ -65,15 +65,20 @@ class DbFileMapper extends QBMapper {
 		$instanceId = $this->config->getSystemValue('instanceid', '');
 
 		$query = $this->db->getQueryBuilder();
-		$query->select('fc.fileid')
+		$query->selectDistinct('fc.fileid')
 			->from('filecache', 'fc')
 			->leftJoin('fc', 'storages', 's', $query->expr()->eq('fc.storage', 's.numeric_id'))
 			->leftJoin(
-				'fc', 'systemtag_object_mapping', 'o', $query->expr()->eq(
-					'o.objectid', $query->createFunction(sprintf('CAST(fc.fileid AS %s)', $this->stringType))))
-			->where($query->expr()->notIn(
-				'o.systemtagid', $query->createNamedParameter($excludedTagIds, IQueryBuilder::PARAM_INT_ARRAY)))
-			->orWhere($query->expr()->isNull('o.systemtagid'))
+				'fc',
+				'systemtag_object_mapping',
+				'o',
+				$query->expr()->andX(
+					$query->expr()->eq('o.objectid', $query->createFunction(sprintf('CAST(fc.fileid AS %s)', $this->stringType))),
+					$query->expr()->eq('o.objecttype', $query->createNamedParameter('files')),
+					$query->expr()->in('o.systemtagid', $query->createNamedParameter($excludedTagIds, IQueryBuilder::PARAM_INT_ARRAY))
+				)
+			)
+			->where($query->expr()->isNull('o.objectid'))
 			->andWhere($query->expr()->neq('fc.mimetype', $query->createNamedParameter($dirMimeTypeId)))
 			->andWhere($query->expr()->orX(
 				$query->expr()->like('fc.path', $query->createNamedParameter('files/%')),
@@ -107,15 +112,20 @@ class DbFileMapper extends QBMapper {
 		$instanceId = $this->config->getSystemValue('instanceid', '');
 
 		$query = $this->db->getQueryBuilder();
-		$query->select('fc.fileid')
+		$query->selectDistinct('fc.fileid')
 			->from('filecache', 'fc')
 			->leftJoin('fc', 'storages', 's', $query->expr()->eq('fc.storage', 's.numeric_id'))
-			->leftJoin(
-				'fc', 'systemtag_object_mapping', 'o', $query->expr()->eq(
-					'o.objectid', $query->createFunction(sprintf('CAST(fc.fileid AS %s)', $this->stringType))))
-			->where($query->expr()->in(
-				'o.systemtagid', $query->createNamedParameter($includedTagIds, IQueryBuilder::PARAM_INT_ARRAY)))
-			->orWhere($query->expr()->isNull('o.systemtagid'))
+			->innerJoin(
+				'fc',
+				'systemtag_object_mapping',
+				'o',
+				$query->expr()->andX(
+					$query->expr()->eq('o.objectid', $query->createFunction(sprintf('CAST(fc.fileid AS %s)', $this->stringType))),
+					$query->expr()->eq('o.objecttype', $query->createNamedParameter('files')),
+					$query->expr()->in('o.systemtagid', $query->createNamedParameter($includedTagIds, IQueryBuilder::PARAM_INT_ARRAY))
+				)
+			)
+			->where($query->expr()->isNotNull('o.objectid'))
 			->andWhere($query->expr()->neq('fc.mimetype', $query->createNamedParameter($dirMimeTypeId)))
 			->andWhere($query->expr()->orX(
 				$query->expr()->like('fc.path', $query->createNamedParameter('files/%')),
